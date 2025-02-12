6af18e248c3e847760e171954666d6a28189ac09
Abhishek-1806 <abhishek458071@gmail.com>
2025-02-12 23:19:18 +0530

    updated files

--- backend/server.js
+++ backend/server.js
@@ -37,21 +37,12 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
-
-app.use(express.static("frontend/dist", {
-    setHeaders: (res, filePath) => {
-        if (filePath.endsWith(".css")) {
-            res.setHeader("Content-Type", "text/css");
-            res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-        }
-    }
-}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
